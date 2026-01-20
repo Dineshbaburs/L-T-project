59953b07900229f0bbc75f9ec2a4b51e9a1628fc
Dinesh <rsdineshbaburajkumar@gmail.com>
2026-01-20 17:22:01 +0530

    Create home.component.ts

---
+++ project/src/app/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  template: `
+    <div style="text-align:center;margin-top:40px">
+      <h2>Welcome 👋</h2>
+      <p>Angular 17 project is running successfully.</p>
+    </div>
+  `
+})
+export class HomeComponent {}
